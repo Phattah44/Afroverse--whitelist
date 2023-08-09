1b5320ab620971e5b074cb84fb4892255085864f
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2023-08-09 19:22:26 +0100

    changed the backend base url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import react from 'react';
 import './App.css';
 import validator from 'validator';
 
@@ -8,25 +7,67 @@ import {
   BiLogoTwitter,
   BiLogoLinkedinSquare,
 } from 'react-icons/bi';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
-  let yourAlert = { message };
+  const BASE_URL = "https://afroverse-whitelist.onrender.com/";
+  const [message, setMessage] = useState({validilityError: '', validitySuccess: ''});
+  const [value, setValue] = useState('');
+  const [submit, setSubmit] = useState(false);
+  const [success, setSuccess] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
   const validateEmail = (e) => {
     e.preventDefault();
     let email = e.target.value;
     if (validator.isEmail(email)) {
-      setMessage(
-        'Thanks for filling out your mail!.....we will be in touch with you'
-      );
+      setIsValid(false);
+      setMessage({...message, validitySuccess: 'valid email...', validilityError: ''});
     } else {
-      setMessage('Please enter a valid email');
+      setIsValid(true);
+      setMessage({...message, validilityError: 'Please enter a valid email and try again...', validitySuccess: ''});
     }
   };
-  const onSubmitEmail = () => {
-    alert(JSON.stringify(yourAlert));
+
+  const onSubmitEmail = async (e) => {
+    e.preventDefault();
+    
+    setMessage('');
+
+    try{
+
+      if(value === "") setIsValid(true);
+
+      const response = await fetch(`${BASE_URL}api/add-awaiting-user`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: value,
+        })
+      })
+
+      const data = await response.json();
+
+      if(data.status === 'ok') {
+        setValue('');
+        setSuccess(data.message);
+        setSubmit(true)
+      }
+  
+    }catch(error){
+      setSuccess(error)
+      setMessage({...message, validilityError: 'Network error: pls check your network and try again', validitySuccess: ''})
+    }
   };
+
+ useEffect(() => {
+  if(submit){
+    setTimeout( () => setSubmit(false), 3000);
+  }
+ }, [submit])
+
   return (
     <div className='whitelist--page'>
       <div className='whitelist--content'>
@@ -39,15 +80,23 @@ function App() {
             A virtual museum and NFT marketplace for African arts, explore,
             mint, auction & trade, and learn about African culture
           </p>
-          <div className='form--input'>
+
+          {submit && (<p style={{background: "#98FB98", color: "black", padding: ".5rem", border: "2px solid green" , borderRadius: "5px"}}>{success}</p>)}
+          
+           {!submit && 
+          (<div className='form--input'>
             <div className='for--input'>
               <BiLogoGmail style={{ fontSize: '20px', color: '#fff' }} />
               <input
                 id='email'
                 type='email'
                 name='email'
+                value={value}
                 placeholder={'Email Address'}
-                onChange={(e) => validateEmail(e)}
+                onChange={(e) => {
+                  validateEmail(e);
+                  setValue(() => e.target.value);
+                }}
                 style={{
                   background: 'none',
                   border: 'none',
@@ -57,10 +106,14 @@ function App() {
                 }}
               />
             </div>
-            <button type='submit' onClick={onSubmitEmail}>
+            {message.validilityError && (<p style={{background: "orange", color: "black", padding: ".5rem", border: "2px solid red" , borderRadius: "5px"}}>{message.validilityError}</p>)}
+            {message.validitySuccess && (<p style={{background: "#98FB98", color: "black", padding: ".5rem", border: "2px solid green" , borderRadius: "5px"}}>{message.validitySuccess}</p>)}
+
+            <button type='submit' style={{cursor: "pointer"}} disabled={isValid} onClick={onSubmitEmail}>
               Join the waitlist
             </button>
-          </div>
+          </div>)}
+          
           <div className='social--icon'>
             <div className='footer--icon'>
               <a href='https://linkedin.com/company/afroverse'>
@@ -87,6 +140,7 @@ function App() {
         <div className='side--image'>
           <img
             src='./pana.png'
+            alt="pana--pic"
             className='page--image'
             style={{ width: '500px' }}
           />
